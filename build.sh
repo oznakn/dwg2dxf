e313f0a3cd8a2a0a8f63965982c5bf907c05bf02
Ozan Akın <ozn.akn@gmail.com>
2022-11-14 17:52:22 +0300

    improvements

--- build.sh
+++ build.sh
@@ -11,17 +11,18 @@ rm -rf .git/
 
 mkdir -p build
 mkdir -p dwg2dxf/build
-echo -e '\n\ninclude_directories(/usr/local/Cellar/libiconv/1.17/include)\nlink_libraries(/usr/local/Cellar/libiconv/1.17/lib PRIVATE)' >> CMakeLists.txt
 
+echo -e "\n\n" >> CMakeLists.txt
+cat ../baseCMakeLists.txt >> CMakeLists.txt
 cp ../dwg2dxfCMakeLists.txt dwg2dxf/CMakeLists.txt
 
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-cmake --build . --verbose --config Release
+cmake --build . --config Release
 cp libdxfrw.a ../dwg2dxf/
 
 cd ../dwg2dxf/build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-cmake --build . --verbose --config Release
+cmake --build . --config Release
 
 cp dwg2dxf ../../../
